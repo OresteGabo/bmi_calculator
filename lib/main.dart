45a1ca1f48e2714e9239b651cdda9a6512a51496
oreste <muhirwa.g.oreste@gmail.com>
2022-08-12 12:05:17 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('This the body'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('this is a floating action button');
+        },
+      ),
+    );
+  }
+}
